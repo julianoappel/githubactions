c24e943592cefe8e6afa422c3c5b2f6edfaee9c9
Juliano Appel <juhappel@hotmail.com>
2021-12-22 14:23:52 -0300

    add my first github action4

--- dist/index.js
+++ dist/index.js
@@ -8452,10 +8452,10 @@ async function run() {
     const { context = {} } = github;
     const { pull_request } = context.payload;
 
-    await octokit.issues.createComment({
+    await octokit.rest.issues.createComment({
         ...context.repo,
         issue_number: pull_request.number,
-        body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.'
+        body: 'Obrigado por enviar uma solicitação de pull! Revisaremos isso o mais rápido possível.'
     });
 }
 
